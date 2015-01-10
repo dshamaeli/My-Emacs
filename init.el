5d9d5e3133d94add0703ea205c490313426158f9
Daniel Shamaeli <daniel.shamaeli@gmail.com>
2015-01-10 15:25:08 +0330

    add js2 ac-js2 Zenburn

--- init.el
+++ init.el
@@ -31,11 +31,11 @@
 ;;;Define default packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defvar daniel/packages '(ac-slime
-                          auto-complete
+(defvar daniel/packages '(ac-js2
+			  ac-slime
+			  auto-complete
                           autopair
                           coffee-mode
-                          csharp-mode
                           deft
                           erlang
                           feature-mode
@@ -46,6 +46,7 @@
                           haml-mode
                           haskell-mode
                           htmlize
+			  js2-mode
 			  js3-mode
 			  json-mode	
                           magit
@@ -68,7 +69,8 @@
                           web-mode
                           writegood-mode
                           yaml-mode
-			  yasnippet)
+			  yasnippet
+ 			  zenburn-theme)
   "Default packages")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -126,35 +128,23 @@
 ;;;Language Hooks
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;web-mode
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-;;;HTML offset indentation
-(setq web-mode-markup-indent-offset 2)
-;;;CSS offset indentation
-(setq web-mode-css-indent-offset 2)
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+    ("9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
